eb39e27ce82f60da1898fb151ab374ad6ce67c1c
y-ohgi <ohgi.211@gmail.com>
2016-02-20 14:43:02 +0900

    Add URLだけ

---
+++ main.js
@@ -0,0 +1,53 @@
+'use strict';
+var
+express = require('express'),
+app = express(),
+port = process.env.PORT || 3000,
+parser = require('body-parser');
+
+app.use(parser.json());
+app.use(express.static(__dirname + '/public'))
+
+
+
+
+app.get('/api/projects', function (req, res, next) {
+    res.json('PONG');
+    return next();
+});
+
+app.get('/api/projects/:id', function (req, res, next) {
+    res.json('PONG');
+    console.log("GET: "+req.params.id);
+    return next();
+});
+
+app.delete('/api/projects/:id', function (req, res, next) {
+    res.json('PONG');
+    console.log("DELETE: "+req.params.id);
+    return next();
+});
+
+
+//======
+app.get('/api/ping', function (req, res, next) {
+    res.json('PONG');
+    return next();
+});
+
+app.get('/api/notfound', function (req, res, next) {
+    res.status(404).json('NotFound');
+    return next();
+});
+
+app.post('/api/badrequest', function (req, res, next) {
+    res.status(400).json('BadRequest');
+    return next();
+})
+/**/
+
+
+
+app.listen(port, function () {
+    console.log('Server running with port', port);
+});
